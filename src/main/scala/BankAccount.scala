8a96dcf06f78734cd73418773ad22b39b727db37
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-09 12:15:50 +0200

    reformatting

--- src/main/scala/BankAccount.scala
+++ src/main/scala/BankAccount.scala
@@ -1,13 +1,10 @@
-trait BankAccount {
+trait BankAccount:
 
-  def closeAccount(): Unit
+   def closeAccount(): Unit
 
-  def getBalance: Option[Int]
+   def getBalance: Option[Int]
 
-  def incrementBalance(increment: Int): Option[Int]
-}
+   def incrementBalance(increment: Int): Option[Int]
 
-object Bank {
-  def openAccount(): BankAccount = ???
-}
-
+object Bank:
+   def openAccount(): BankAccount = ???
