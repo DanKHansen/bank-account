fddd68cc479cd280c41d87fde05a9ff12fe1b834
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-09 15:49:14 +0200

    refactoring

--- src/main/scala/BankAccount.scala
+++ src/main/scala/BankAccount.scala
@@ -7,17 +7,12 @@ object Bank:
    def openAccount(): BankAccount = new BankAccount:
       private var (bal, isOpen): (Int, Boolean) = (0, true)
 
-      override def closeAccount(): Unit = this.synchronized {
-         isOpen = false
-      }
+      override def closeAccount(): Unit = this.synchronized { isOpen = false }
 
-      override def getBalance: Option[Int] = this.synchronized {
-         if isOpen then Some(bal) else None
-      }
+      override def getBalance: Option[Int] = this.synchronized { if isOpen then Some(bal) else None }
 
-      override def incrementBalance(increment: Int): Option[Int] = this.synchronized {
+      override def incrementBalance(increment: Int): Option[Int] = this.synchronized:
          if isOpen then
             bal += increment
             Some(bal)
          else None
-      }
